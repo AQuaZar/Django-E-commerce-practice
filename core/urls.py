02f93c7ef2a8473c5e251a5f332cbf8f8743cbce
AQuaZar <greenskullcompany@gmail.com>
2020-07-15 19:47:16 +0300

    Add logic for deleting items from cart

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import HomeView, ItemDetailView, checkout, detail, add_to_cart
+from .views import (
+    HomeView,
+    ItemDetailView,
+    checkout,
+    detail,
+    add_to_cart,
+    remove_from_cart,
+)
 
 
 app_name = "core"
@@ -9,5 +16,6 @@ urlpatterns = [
     path("checkout", checkout, name="checkout-page"),
     path("product/<slug>/", ItemDetailView.as_view(), name="product"),
     path("add-to-cart/<slug>", add_to_cart, name="add-to-cart"),
+    path("remove-from-cart/<slug>", remove_from_cart, name="remove-from-cart"),
 ]
 

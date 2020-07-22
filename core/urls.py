15dedad8c66113c516b3f565be052a78ca4a68d6
AQuaZar <greenskullcompany@gmail.com>
2020-07-22 20:39:21 +0300

    Complete views, form and template for checkout feature

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (
     HomeView,
     ItemDetailView,
-    checkout,
+    CheckoutView,
     detail,
     add_to_cart,
     remove_from_cart,
@@ -15,7 +15,7 @@ app_name = "core"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home-page"),
-    path("checkout", checkout, name="checkout-page"),
+    path("checkout", CheckoutView.as_view(), name="checkout-page"),
     path("order-summary", OrderSummaryView.as_view(), name="order-summary"),
     path("product/<slug>/", ItemDetailView.as_view(), name="product"),
     path("add-to-cart/<slug>", add_to_cart, name="add-to-cart"),
